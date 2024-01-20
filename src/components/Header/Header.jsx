94cb137e8740da5c3a779087e737105fc8e614e2
lahiruyasas7 <lahiruyasas7@gmail.com>
2024-01-20 23:57:09 +0530

    header-and-navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./Header.css";
+import  NavBar  from "../navbar/Navbar";
 
 const Header = () => {
   return (
-    <header>
-      <div className='wrapper'>
-        <p className='course-name'>Sound Design MasterClass</p>
+    <header className="wrapper">
+      <div style={{justifyContent: 'center', display: 'flex'}}><NavBar /></div>
+      
+      <div className="cta">
+        <p className="course-name">Sound Design MasterClass</p>
+        <div className="red-bar"></div>
         <h1>Learn the art of sound track</h1>
-        <a href='#' className='demo-btn'>Demo Lesson</a>
+        <a href="#" className="demo-btn">
+          Demo Lesson
+        </a>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  return <div className="nav">
+    <div><a href="#" className="logo">Sound<span>DZing</span></a></div>
+    <div className="nav-items" style={{display: 'flex', gap: '50px'}}>
+      <a href="#">CourseDetails</a>
+      <a href="#">About</a>
+      <a href="#">Blog</a>
+      <a href="#">Testimonials</a>
+    </div>
+  </div>;
+};
 
-export default Navbar
+export default Navbar;
